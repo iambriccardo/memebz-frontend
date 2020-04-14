d86691b0182419d740a5dc1d413659fa6f4a45c5
RiccardoBusetti <riccardob36@gmail.com>
2020-04-14 09:13:52 +0200

    Move from Blueprint to Material UI

--- src/meme/memecard/MemeCard.js
+++ src/meme/memecard/MemeCard.js
@@ -1,18 +1,94 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MemeCard.css'
-import {Card, Elevation,Text} from "@blueprintjs/core";
+import {makeStyles} from '@material-ui/core/styles';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import {red} from '@material-ui/core/colors';
+import ChatRoundedIcon from '@material-ui/icons/ChatRounded';
+import Collapse from '@material-ui/core/Collapse';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ArrowUpwardRoundedIcon from '@material-ui/icons/ArrowUpwardRounded';
+import ArrowDownwardRoundedIcon from '@material-ui/icons/ArrowDownwardRounded';
+import Typography from "@material-ui/core/Typography";
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {},
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    avatar: {
+        backgroundColor: red[500],
+    },
+}));
 
 function MemesCard({meme}) {
+    const classes = useStyles();
+
+    const [isExpanded, expand] = useState(false);
+
     return (
-        <Card className="card" interactive={true} elevation={Elevation.TWO}>
-            {console.log(meme)}
-            <Text tagName="h3" ellipsize={true}>
-                {meme.title}
-            </Text>
-            <Text tagName="p" ellipsize={true}>
-                {meme.author}
-            </Text>
-            <img src={meme.imageUrl} alt={meme.imageUrl}/>
+        <Card className={classes.root}>
+            <CardHeader
+                avatar={
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                        {meme.author[0]}
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon/>
+                    </IconButton>
+                }
+                title={meme.title}
+                subheader={meme.dateCreated}
+            />
+            <CardMedia
+                className={classes.media}
+                image={meme.imageUrl}
+                title="Paella dish"
+            />
+            <CardContent>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="up vote meme">
+                    {meme.upVotes}
+                    <ArrowUpwardRoundedIcon/>
+                </IconButton>
+                <IconButton aria-label="down vote meme">
+                    {meme.downVotes}
+                    <ArrowDownwardRoundedIcon/>
+                </IconButton>
+                <IconButton
+                    onClick={e => expand(!isExpanded)}
+                    aria-label="show comments"
+                >
+                    <ChatRoundedIcon/>
+                </IconButton>
+            </CardActions>
+            <Collapse in={isExpanded} timeout="auto" unmountOnExit>
+                <CardContent>
+                    <Typography variant="body1">
+                        Comments are still under implementation, sorry for the inconvenience.
+                    </Typography>
+                </CardContent>
+            </Collapse>
         </Card>);
 }
 
--- src/meme/memeslist/MemesList.js
+++ src/meme/memeslist/MemesList.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import { useQuery } from '@apollo/react-hooks';
+import {useQuery} from '@apollo/react-hooks';
 import MemesCard from "../memecard/MemeCard";
+import Grid from '@material-ui/core/Grid';
+import {makeStyles} from "@material-ui/core/styles";
 
 const GET_MEMES = gql`
   {
@@ -9,20 +11,30 @@ const GET_MEMES = gql`
         title
         author
         imageUrl
+        dateCreated
+        upVotes
+        downVotes
     }
   }
 `;
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: 16
+    }
+}));
+
 function MemesList() {
-    const { loading, error, data } = useQuery(GET_MEMES);
+    const classes = useStyles();
+    const {loading, error, data} = useQuery(GET_MEMES);
 
     if (loading) return 'Loading...';
     if (error) return `Error! ${error}`;
 
     return (
-        <>
-            {data.memes.map(meme => <MemesCard meme={meme} />)}
-        </>
+        <Grid className={classes.root} container direction="row" justify="center" alignItems="center" spacing={2}>
+            {data.memes.map(meme => <Grid item xs={6}><MemesCard meme={meme}/></Grid>)}
+        </Grid>
     );
 }
 
